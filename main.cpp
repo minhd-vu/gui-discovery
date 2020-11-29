af7a0c4010679ba6d918a1cc727ddd9fd3fded8b
Minh Vu <minhd_vu@yahoo.com>
2020-11-29 06:26:19 +0000

    added drag and drop todo list

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <QtWidgets>
+#include <QApplication>
 #include "mainwidget.h"
+#include "todolist.h"
 
 int main(int argc, char *argv[])
 {
@@ -7,8 +9,11 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
 
     // This is our MainWidget class containing our GUI and functionality
-    MainWidget w;
-    w.show(); // Show main window
+    // MainWidget w;
+    // w.show(); // Show main window
+
+    CToDoList app;
+    app.show();
 
     // run the application and return execs() return value/code
     return a.exec();
